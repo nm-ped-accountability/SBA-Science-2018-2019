03cacf1267fff7deb3560e263df0c15d9c436dc7
yaoyunhz <yaoyunhz@gmail.com>
2019-06-27 09:45:26 -0600

    initial commit

---
+++ SBA Science SOAP and Web Files 2018-2019.R
@@ -0,0 +1,6 @@
+
+## SBA Science 2018-2019 SOAP and Web Files
+
+sba <- read.csv("ADDED DEMO_NMSBA1819Admin1StudentResultsUNMASKED_Updated 06202019.csv",
+                header = TRUE, stringsAsFactors = FALSE)
+head(sba)
